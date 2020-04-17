03245895ccf58ee67e46276fe0d507d564484c09
unknown <lvova.nikol@mail.ru>
2020-04-17 16:13:17 +0300

    Начало работы

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dz13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
